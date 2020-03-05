616c873e744737dd5d14f3c5f72906b8b7a0faf0
Anshul Hajare <anshulhajare09@gmail.com>
2020-03-05 18:31:17 +0530

    update serverr.js

--- serverr.js
+++ serverr.js
@@ -1,37 +1,46 @@
 var http = require('http');
 var url = require('url');
-let static = require('node-static');
-let fileServer = new static.Server('.');
+var static = require('node-static');
 
-var http = require('http');
-var arr = [];
-function sendToAll(message) {
-    arr.forEach( (item,index) => {
-        item.end(message);
-    });
+var fileServer = new static.Server('.');
+
+var subscribers = Object.create(null);
+
+function onSubscribe(req, res) {
+  var id = Math.random();
+
+  res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+  res.setHeader("Cache-Control", "no-cache, must-revalidate");
+  subscribers[id] = res;
+
+  req.on('close', function() {
+    delete subscribers[id];
+  });
+}
+
+function publish(message) {
+  for (var id in subscribers) {
+    var res = subscribers[id];
+    res.end(message);
+  }
 }
 
 http.createServer(function (req, res) {
-    console.log('Server running on port 8080');
-    let parsedInfo = url.parse(req.url, true);
-    if(parsedInfo.pathname == '/request' && parsedInfo.method == 'POST'){
-        req.setEncoding('utf8');
-        let message = '';
-        req.on('data', (value) => {
-            message += value;
-        }).on('end', function() {
-            sendToAll(message); 
-            res.end("ok");
-        });
-        return;
-    }
-    if(parsedInfo.pathname == '/callServer'){
-        res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-        res.setHeader("Cache-Control", "no-cache, must-revalidate");
-        arr.insert(res);
-        console.log(arr);
-        return;
-    }
-    fileServer.serve(req, res);
-}).listen(8080);
-
+  var urlParsed = url.parse(req.url, true);
+  if (urlParsed.pathname == '/subscribe') {
+    onSubscribe(req, res);
+    return;
+  }
+  if (urlParsed.pathname == '/publish' && req.method == 'POST') {
+    req.setEncoding('utf8');
+    var message = '';
+    req.on('data', function(chunk) {
+      message += chunk;
+    }).on('end', function() {
+      publish(message);
+      res.end("ok");
+    });
+    return;
+  }
+  fileServer.serve(req, res);
+}).listen(8085);
